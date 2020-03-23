6827cfbf56639f4ccd4ab2c2d6cb05258c9cbb9c
rinchannow <691182679@qq.com>
2020-03-23 19:59:07 +0800

    Exercise 2 finished.

--- HomeworkTwo/homework/src/main/java/chapter/android/aweme/ss/com/homework/Exercises2.java
+++ HomeworkTwo/homework/src/main/java/chapter/android/aweme/ss/com/homework/Exercises2.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.LinkedList;
 
 /**
  * 作业2：一个抖音笔试题：统计页面所有view的个数
@@ -16,10 +20,32 @@ public class Exercises2 extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.exercise2);
+
+        TextView centerTextView = findViewById(R.id.tv_center);
+        ViewGroup allViews = findViewById(R.id.all_views);
+        centerTextView.setText(String.valueOf(getAllChildViewCount(allViews)));
     }
 
-    public int getAllChildViewCount(View view) {
-        //todo 补全你的代码
-        return 0;
+    public int getAllChildViewCount(ViewGroup view) {
+        // BFS 或者 DFS，我不想递归那就 BFS 吧
+        int count = 0;
+        if (view == null || view.getChildCount() < 1)
+            return 0;
+        LinkedList<ViewGroup> queue = new LinkedList<>();
+        queue.add(view);
+        while (!queue.isEmpty()) {
+            ViewGroup cur = queue.removeFirst();
+            if (cur != view)
+                count += 1;
+            for (int i = 0; i < cur.getChildCount(); i++) {
+                if (cur.getChildAt(i) instanceof ViewGroup) {
+                    queue.addLast((ViewGroup) cur.getChildAt(i));
+                } else {
+                    count += 1;
+                }
+            }
+        }
+        return count;
     }
 }
--- README.md
+++ README.md
@@ -5,13 +5,16 @@
 1. 使用至少五种视图控件。
 2. 实现简单交互。
 
-我使用了 `ImageView`, `Switch`, `TextView`, `RadioGroup`, `RadioButton`, `Button`。
+	**解题过程**：我使用了 `ImageView`, `Switch`, `TextView`, `RadioGroup`, `RadioButton`, `Button`。
 
 ## 2020/3/23 作业二
 
 1. Logcat在屏幕旋转的时候 `onStop()` 和 `onDestroy()` 会展示出来，但UI界面我们看不到，在今天课程基础上想办法补全它，让其跟 Logcat 的展示一样。
 
-	解题思路：本题使用比 `Acitivity` 生命周期更长的 `Application` 来保存 `onSaveInstanceState()` 之后的 Log，并在重启之后将 Log 字符串返回给 `TextView`。
+	**解题过程**：本题使用比 `Acitivity` 生命周期更长的 `Application` 来保存 `onSaveInstanceState()` 之后的 Log，并在重启之后将 Log 字符串返回给 `TextView`。
 
 2. 一个抖音笔试题：统计页面所有 view 的个数。ViewGroup 中的 API: `getChildCount()`, `getChildAt()`。
+
+	**解题过程**：整个 `ViewGroup` 其实就是一个广义表，`ViewGroup` 中可以继续有 `ViewGroup` 或 `View`（当然也可以为空）。所以要遍历广义表（或者可以称之为多叉树，因为一开始有一个根），可以采用广度优先搜索或深度优先搜索。在这里我不想用递归，所以采用了广度优先搜索。
+
 3. 实现一个类似抖音消息页面，并且点击每个 recycleview 的 item，能够跳转到一个新的界面，并且在新的页面显示出他是第几个 item。
