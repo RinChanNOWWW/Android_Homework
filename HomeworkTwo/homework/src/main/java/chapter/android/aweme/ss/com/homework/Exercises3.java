755308309116311b7839e45a9b129af1ae4832ea
rinchannow <691182679@qq.com>
2020-03-23 10:22:08 +0800

    更新作业二

---
+++ HomeworkTwo/homework/src/main/java/chapter/android/aweme/ss/com/homework/Exercises3.java
@@ -0,0 +1,40 @@
+package chapter.android.aweme.ss.com.homework;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * 大作业:实现一个抖音消息页面,
+ * 1、所需的data数据放在assets下面的data.xml这里，使用PullParser这个工具类进行xml解析即可
+ * <p>如何读取assets目录下的资源，可以参考如下代码</p>
+ * <pre class="prettyprint">
+ *
+ *         @Override
+ *     protected void onCreate(@Nullable Bundle savedInstanceState) {
+ *         super.onCreate(savedInstanceState);
+ *         setContentView(R.layout.activity_xml);
+ *         //load data from assets/data.xml
+ *         try {
+ *             InputStream assetInput = getAssets().open("data.xml");
+ *             List<Message> messages = PullParser.pull2xml(assetInput);
+ *             for (Message message : messages) {
+ *
+ *             }
+ *         } catch (Exception exception) {
+ *             exception.printStackTrace();
+ *         }
+ *     }
+ * </pre>
+ * 2、所需UI资源已放在res/drawable-xxhdpi下面
+ *
+ * 3、作业中的会用到圆形的ImageView,可以参考 widget/CircleImageView.java
+ */
+public class Exercises3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
---
+++ HomeworkTwo/homework/src/main/java/chapter/android/aweme/ss/com/homework/model/Message.java
@@ -0,0 +1,65 @@
+package chapter.android.aweme.ss.com.homework.model;
+
+
+/**
+ * 消息  data class
+ */
+public class Message {
+
+    private boolean isOfficial;//是否官方
+    private String title;//title
+    private String time;//时间
+    private String description;//描述
+    private String icon;//icon
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public boolean isOfficial() {
+        return isOfficial;
+    }
+
+    public void setOfficial(boolean official) {
+        isOfficial = official;
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "isOfficial=" + isOfficial +
+                ", title='" + title + '\'' +
+                ", time='" + time + '\'' +
+                ", description='" + description + '\'' +
+                ", icon='" + icon + '\'' +
+                '}';
+    }
+}
