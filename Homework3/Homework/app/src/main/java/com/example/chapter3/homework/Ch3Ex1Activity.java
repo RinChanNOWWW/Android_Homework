1dd57b957c6ac53801b534c6dd177831b2903139
rinchannow <691182679@qq.com>
2020-03-29 12:26:02 +0800

    add homework 3

---
+++ Homework3/Homework/app/src/main/java/com/example/chapter3/homework/Ch3Ex1Activity.java
@@ -0,0 +1,58 @@
+package com.example.chapter3.homework;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.SeekBar;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class Ch3Ex1Activity extends AppCompatActivity {
+    private LottieAnimationView animationView;
+    private CheckBox loopCheckBox;
+    private SeekBar seekBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ch3ex1);
+
+        animationView = findViewById(R.id.animation_view);
+        loopCheckBox = findViewById(R.id.loop_checkbox);
+        seekBar = findViewById(R.id.seekbar);
+
+        loopCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    // 当选中自动播放的时候，开始播放 lottie 动画，同时禁止手动修改进度
+                    animationView.playAnimation();
+                    seekBar.setEnabled(false);
+                } else {
+                    // 当去除自动播放时，停止播放 lottie 动画，同时允许手动修改进度
+                    animationView.pauseAnimation();
+                    seekBar.setEnabled(true);
+                }
+            }
+        });
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                // TODO ex1-2: 这里应该调用哪个函数呢
+                // 提示1：可以参考 https://airbnb.io/lottie/#/android?id=custom-animators
+                // 提示2：SeekBar 的文档可以把鼠标放在 OnProgressChanged 中间，并点击 F1 查看，
+                // 或者到官网查询 https://developer.android.google.cn/reference/android/widget/SeekBar.OnSeekBarChangeListener.html#onProgressChanged(android.widget.SeekBar,%20int,%20boolean
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+    }
+}
---
+++ Homework3/Homework/app/src/main/java/com/example/chapter3/homework/Ch3Ex3Activity.java
@@ -0,0 +1,27 @@
+package com.example.chapter3.homework;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 使用 ViewPager 和 Fragment 做一个简单版的好友列表界面
+ * 1. 使用 ViewPager 和 Fragment 做个可滑动界面
+ * 2. 使用 TabLayout 添加 Tab 支持
+ * 3. 对于好友列表 Fragment，使用 Lottie 实现 Loading 效果，在 5s 后展示实际的列表，要求这里的动效是淡入淡出
+ */
+public class Ch3Ex3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ch3ex3);
+
+
+
+        // TODO: ex3-1. 添加 ViewPager 和 Fragment 做可滑动界面
+
+
+
+        // TODO: ex3-2, 添加 TabLayout 支持 Tab
+    }
+}
