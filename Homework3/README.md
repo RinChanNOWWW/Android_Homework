e7c32f5fd3845d9f0bdffb821cc1ce40dd14fa91
rinchannow <691182679@qq.com>
2020-03-30 11:26:46 +0800

    Finished ex1

--- Homework3/README.md
+++ Homework3/README.md
@@ -1,1 +1,29 @@
-# Chapter-3
+## 2020/3/29 作业三
+
+1. 引入 Lottie 库实现简单的图标动画
+
+   1. 在 activity_main.xml 中添加 `LottieAnimationView`
+2. 在 `SeekBar` 的回调中修改 `LottieAnimationView` 的进度
+   
+
+
+
+​	**演示效果**
+
+<img src="pics/ex1.gif" style="zoom:33%;" />
+
+1. 使用属性动画，练习 `AnimatorSet` 和 `scale`/`fade` 等基本动画样式
+
+   1. 添加 `scale` 动画
+
+   2. 添加 `alpha` 动画
+
+   3. 组合到 `AnimatorSet` 中
+
+2. 使用 `ViewPager` 和 `Fragment` 做一个简单版的好友列表界面
+
+   1. 使用 `ViewPager` 和 `Fragment` 做个可滑动界面
+
+   2. 使用 `TabLayout` 添加 Tab 支持
+
+   3. 对于好友列表 `Fragment`，使用 Lottie 实现 Loading 效果，在 5s 后展示实际的列表，要求这里的动效是淡入淡出
--- Homework3/app/src/main/java/com/example/chapter3/homework/Ch3Ex1Activity.java
+++ Homework3/app/src/main/java/com/example/chapter3/homework/Ch3Ex1Activity.java
@@ -1,5 +1,6 @@
 package com.example.chapter3.homework;
 
+import android.animation.ValueAnimator;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.CheckBox;
@@ -21,7 +22,8 @@ public class Ch3Ex1Activity extends AppCompatActivity {
         animationView = findViewById(R.id.animation_view);
         loopCheckBox = findViewById(R.id.loop_checkbox);
         seekBar = findViewById(R.id.seekbar);
-
+        loopCheckBox.setChecked(true);
+        seekBar.setEnabled(false);
         loopCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -33,6 +35,8 @@ public class Ch3Ex1Activity extends AppCompatActivity {
                     // 当去除自动播放时，停止播放 lottie 动画，同时允许手动修改进度
                     animationView.pauseAnimation();
                     seekBar.setEnabled(true);
+                    // 定位到当前进度
+                    seekBar.setProgress((int) (animationView.getProgress() * 100));
                 }
             }
         });
@@ -40,10 +44,11 @@ public class Ch3Ex1Activity extends AppCompatActivity {
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                // TODO ex1-2: 这里应该调用哪个函数呢
+                // ex1-2: 这里应该调用哪个函数呢
                 // 提示1：可以参考 https://airbnb.io/lottie/#/android?id=custom-animators
                 // 提示2：SeekBar 的文档可以把鼠标放在 OnProgressChanged 中间，并点击 F1 查看，
                 // 或者到官网查询 https://developer.android.google.cn/reference/android/widget/SeekBar.OnSeekBarChangeListener.html#onProgressChanged(android.widget.SeekBar,%20int,%20boolean
+                animationView.setProgress(progress / 100.0f);
             }
 
             @Override
